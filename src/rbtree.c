6041d5d8a63a89e5991e3c50a08e7d5e8e453664
Ubuntu <ubuntu@ip-172-31-15-249.us-east-2.compute.internal>
2021-12-04 08:22:50 +0000

    bst_erase

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,6 +2,91 @@
 #include <stdlib.h>
 #include <stdio.h> // 나중에 삭제
 
+
+int bst_erase(rbtree *t, node_t* z){
+  if (z->left == NULL){
+    bst_transparent(t,z, z->right);
+  }
+  else if (z->right ==NULL){
+    bst_transparent(t,z,z->left);
+  }
+  else{
+    node_t *y = successor(t, z);
+
+    // z가 y의 부모가 아닌 경우
+    if(y->parent != z){
+      bst_transparent(t, y, y->right);
+      y->right = z->right;
+      y->right->parent = y;
+
+    }
+    bst_transparent(t, z, y);
+    y->left = z->left;
+    y->left->parent = y;
+  }
+  free(z);
+  return 0;
+}
+
+void bst_transparent(rbtree* t, node_t* u, node_t* v){
+  // u가 루트인 경우  
+  if (u->parent == NULL)
+    t->root = v;
+
+  else if (u == u->parent->left)
+    u->parent->left= v;
+    
+  else
+    u->parent->right = v; 
+    
+  if (v!=NULL)
+    v->parent = u->parent;
+}
+
+
+node_t *successor(rbtree *t , node_t *x){
+  // right subtree
+  if (x->right!=NULL){
+    // right subtree의 max
+    x = x->right;
+    while(x->left!=NULL)
+      x = x->left;
+    return x;
+  }
+
+  else{
+    // North - West
+    node_t* y  = x->parent;
+    while (y!=NULL && x == y->right){
+      x = y;
+      y = y->parent;
+    }
+  return y;
+  }
+}
+
+node_t *predecessor(rbtree *t, node_t *x){
+  // left subtree
+  if (x->left != NULL){
+    // left subtree의 max
+    x = x->left;
+    while (x->right!=NULL)
+      x = x->right;
+    return x;
+  }
+
+  else{
+    // North - East
+    node_t* y = x->parent;
+    while (y!=NULL && x == y->left){
+      x = y;
+      y = y->parent;
+    }
+    return y;
+  }
+}
+
+
 void left_rotation(rbtree *t, node_t *x){
   node_t *y = x->right;
   x->right = y->left;
@@ -116,6 +201,15 @@ void preorder(rbtree *t, node_t *p){
   printf("key : %d color : %d, ", p->key, p->color);
 }
 
+
+void delete_node_all(node_t* p){
+  if (p==NULL)
+    return;
+  delete_node_all(p->left);
+  delete_node_all(p->right);
+  free(p);
+}
+
 /* 위에는 내가 필요에 의해 구현 한 것 들*/
 
 rbtree *new_rbtree(void) {
@@ -125,9 +219,11 @@ rbtree *new_rbtree(void) {
   return p;
 }
 
+
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
+  delete_node_all(t->root); -//함수 안쓰고 재귀로 ..? 어떻게 해야하지
   free(t);
+  t=NULL;
 }
 
 // key 추가
@@ -173,7 +269,7 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
     cur->right = node;
   }
 
-  insert_fixup(t, node);
+  // insert_fixup(t, node);
 
   return t->root;
 }
@@ -204,26 +300,34 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
 }
 
 
-// 최소값을 가진 node pointer 반환
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
-  // 맨 왼쪽 거를 반환하면 된다.
-  return t->root;
+  node_t *x = t->root;
+  if (x==NULL){
+    // No node
+    return x;
+  }
+  while (x->left!=NULL)
+    x = x->left;
+  return x;
 }
 
-// 최대값을 가진 node pointer 반환
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
-  return t->root;
+  node_t *x = t->root;
+  if (x==NULL){
+    // No node
+    return x;
+  }
+
+  while (x->right !=NULL)
+    x = x->right;
+  return x;
 }
 
 
 // p로 지정된 node 삭제 및 메모리 반환
 int rbtree_erase(rbtree *t, node_t *p) {
   // TODO: implement erase
-  // rbtree_erase(t, rbtree_find(t,key)); 이런식으로 쓸 수 있을 듯
-  // 
-  return 0;
+    return 0;
 }
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
